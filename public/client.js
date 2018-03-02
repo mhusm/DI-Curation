fea4f43fd9efc75215001cc90317beac857bf49e
Maria Husmann <maria.husmann@gmail.com>
2018-03-02 09:55:56 +0100

    sorting and filtering, basic example

--- public/client.js
+++ public/client.js
@@ -1,31 +1,51 @@
 var app = new Vue({
     el: '#app',
     data: {
-      photos: photos,
-      photos1: photos,
-      photos2: photos,
-      photos3: photos,
-      photos4: photos,
-      photos5: photos,
-      photos6: photos
+      photosOrig: photos,
+      photos: photos.slice(0, photos.length),
+      orderDesc: true,
+      filterX: false
+    },
+    watch: {
+      // whenever filterX changes, this function will run
+      filterX: function (newFilterX, oldFilterX) {
+        if (newFilterX) {
+          this.photos = this.photos.filter(photo => {
+            return photo.boolean;
+          });
+        }
+        else {
+          this.photos = this.photosOrig.slice(0, this.photosOrig.length);
+          this.sort();
+        }
+      }
     },
     methods: {
-      randomize: function () {
-        var p = this.photos3;
-        this.photos3 = this.photos2;
-        this.photos2 = p;
-      },
       transition: function (event) {
         console.log(event)
         event.target.classList.toggle("test");
+      },
+      switchSort: function() {
+        this.orderDesc = !this.orderDesc;
+        this.sort();
+      },
+      sort: function() {
+        if (this.orderDesc) {
+          // Photos sortieren
+          this.photos.sort((a,b) => {
+            return b.rank - a.rank ; //nach "rank", absteigend
+          });
+        } else {
+          // Photos sortieren
+          this.photos.sort((a,b) => {
+            return a.rank - b.rank ; //nach "rank", aufsteigend
+          });
         }
+      }
     },  
     created: function () {
-      this.photos1 = photos.slice(0, 5);
-      this.photos2 = photos.slice(5, 15);
-      this.photos3 = photos.slice(15, 27);
-      this.photos4 = photos.slice(27, 37);
-      this.photos5 = photos.slice(37, 45);
-      this.photos6 = photos.slice(45, 50);
+      this.sort();
+      console.log(this.photos);
+
     }
 });
